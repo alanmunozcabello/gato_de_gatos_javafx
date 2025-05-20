107dfb2c81846576ac384e30031d316cb8d83342
Alan Muñoz <167464537+alanmunozcabello@users.noreply.github.com>
2025-05-20 18:37:15 -0400

    cambios a clase Gato

--- Gato_de_Gatos/Gato.java
+++ Gato_de_Gatos/Gato.java
@@ -2,31 +2,31 @@ public class Gato {
     private char[][] gato = new char[3][3];
     String estado; //"libre", "X", "O", "empate"
 
-    public Gato(){
+    public Gato(){ //crear gato vacio
         for(int i = 0; i < 3; i++){
             for(int j = 0; j < 3; j++){
-                gato[i][j] = ' ';
+                this.gato[i][j] = ' ';
             }
         }
-        estado = "libre";
+        this.estado = "libre";
     }
 
-    public boolean verificarCasillaOcupada(int fila, int columna){
+    public boolean verificarCasillaOcupada(int fila, int columna){ //verifica si la casilla esta ocupada
         if(gato[fila][columna] == ' '){
             return true;
         }
         return false;
     }
 
-    public void marcarSimbolo(int fila, int columna, char simbolo){
+    public void marcarSimbolo(int fila, int columna, char simbolo){ //marca la casilla con el simbolo del jugador
         gato[fila][columna] = simbolo;
     }
 
-    public String getEstado(){
-        return estado;
+    public String getEstado(){ //para saber el estado del gato
+        return this.estado;
     }
 
-    public char[][] getGato(){
+    public char[][] getGato(){ //retorna el gato completo
         return this.gato;
     }
     
